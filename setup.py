57f0472a6dece76d734fee6cdc32cf406e5983d8
Shadi Akiki <shadiakiki1986@gmail.com>
2017-09-07 16:04:03 +0000

    move requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,23 @@
 from distutils.core import setup
+
 setup(name='keras_models_factory',
       version='0.1',
       packages=['keras_models_factory'],
+      install_requires=[
+        "sklearn",
+        "pandas",
+        "numpy",
+        "scipy",
+        "matplotlib",
+
+        # until https://github.com/fchollet/keras/pull/7566/files/f2b66a02067cd5a0bc7291231c5fe59f355ff2ad#r134733445
+        # use keras 2.0.6 and not 2.0.7
+        "Keras==2.0.6",
+
+        "tensorflow",
+        "h5py",
+        "scikit-image",
+        "jupyter",
+        "tensorboard"
+      ]
       )
