bd05280aef39e1a0bbebeb46eeb13e66cb3a1033
Shadi Akiki <shadiakiki1986@gmail.com>
2017-09-15 08:10:41 +0000

    fixup

--- tests/test_lstm_ae.py
+++ tests/test_lstm_ae.py
@@ -1,6 +1,6 @@
 from  keras_models_factory import lstm_ae, datasets
 
-from test_base import TestBase, read_params_yml
+from test_base import read_params_yml
 from test_lstm_base import TestLstmBase
 
 import os 
@@ -22,6 +22,12 @@ class TestLstmAutoencoder(TestLstmBase):
       data_cb = lambda: datasets.ds_1(nb_samples=nb_samples, look_back=look_back, seed=42)
 
       fit_kwargs = self._data(fit_kwargs, data_cb, look_back)
+
+      # since AE
+      fit_kwargs['y'] = fit_kwargs['x']
+      Xc_test, _ = fit_kwargs['validation_data']
+      fit_kwargs['validation_data'] = (Xc_test,Xc_test)
+
       model_callback = lambda: lstm_ae.model_1(fit_kwargs['x'].shape[2], mkw['lstm_dim'], mkw['look_back'], mkw['enc_dim'], fit_kwargs['x'].shape[2])
 
       f = lambda *args: self.assert_fit_model(*args)
